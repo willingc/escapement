a02cc5f4367acf6cbc7f0734744b5093b4b02597
Carol Willing <carolcode@willingconsulting.com>
2021-01-27 00:36:48 -0800

    adjust the function for a pandas df

--- escapement/report.py
+++ escapement/report.py
@@ -16,6 +16,6 @@ def create_dataframe(result):
     print(result)
     result_json = json.dumps(result)
     with open('results.json', 'w') as json_file:
-        json.dump(result_json, json_file)
+        json.dump(result, json_file)
 
 
